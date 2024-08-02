c9fe23143ade472e2b955de56810af0a674088ae
Beatriz Belaparte Favero <beatrizfavero-ieg@instituto.ad>
2024-08-02 17:04:34 -0300

    Melhorando mq

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/', async (req, res) => {
     const client = await pool.connect();
 
     try {
-        const info = await client.query(`SELECT t.turma, COALESCE(p.presentes, 0) AS "presentes", COALESCE(a.ausentes, 0) AS "ausentes", t.total FROM (SELECT c.turma, COUNT(c.turma) AS "total" FROM candidatos c GROUP BY c.turma) t LEFT JOIN   (SELECT c.turma, COUNT(c.turma) AS "presentes" FROM         candidatos c JOIN presenca p ON c.nr_inscricao = p.nr_inscricao WHERE p.data_presenca = current_date GROUP BY c.turma ) p ON t.turma = p.turma LEFT JOIN (SELECT c.turma,  COUNT(c.turma) AS "ausentes" FROM candidatos c WHERE c.nome NOT IN (SELECT c.nome FROM presenca p RIGHT JOIN candidatos c ON c.nr_inscricao = p.nr_inscricao WHERE p.data_presenca = current_date) GROUP BY c.turma) a ON t.turma = a.turma;`);
+        const info = await client.query(`SELECT t.turma, COALESCE(p.presentes, 0) AS "presentes", COALESCE(a.ausentes, 0) AS "ausentes", t.total FROM (SELECT c.turma, COUNT(c.turma) AS "total" FROM candidatos c GROUP BY c.turma) t LEFT JOIN   (SELECT c.turma, COUNT(c.turma) AS "presentes" FROM         candidatos c JOIN presenca p ON c.nr_inscricao = p.nr_inscricao WHERE p.data_presenca = current_date GROUP BY c.turma ) p ON t.turma = p.turma LEFT JOIN (SELECT c.turma,  COUNT(c.turma) AS "ausentes" FROM candidatos c WHERE c.nome NOT IN (SELECT c.nome FROM presenca p RIGHT JOIN candidatos c ON c.nr_inscricao = p.nr_inscricao WHERE p.data_presenca = current_date) GROUP BY c.turma) a ON t.turma = a.turma order by t.turma;`);
 
         res.render('index', { info: info.rows });
         client.release();
